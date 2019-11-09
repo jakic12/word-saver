637833becc1696e9a8272987ccd7756edbb31987
jakob <drusany.jakob@gmail.com>
2019-11-09 16:57:48 +0100

    Updates

---
+++ precache-manifest.5ee8ff81c94a30e7d808eaaea1ebe8b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7eca8243039f545ea3a741316af318c",
+    "url": "/word-saver/index.html"
+  },
+  {
+    "revision": "fc195dd9090136bc34b7",
+    "url": "/word-saver/static/css/main.f3e2a2aa.chunk.css"
+  },
+  {
+    "revision": "8483391b98b484d07843",
+    "url": "/word-saver/static/js/2.c2c605b3.chunk.js"
+  },
+  {
+    "revision": "fc195dd9090136bc34b7",
+    "url": "/word-saver/static/js/main.a99a4fd7.chunk.js"
+  },
+  {
+    "revision": "19323843d2921b256526",
+    "url": "/word-saver/static/js/runtime-main.91720b33.js"
+  }
+]);
